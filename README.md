57fb4dbaafbabef6e665da166d244e5cae8e9e0d
Ilmam Fauzi <ilmam.javajin@gmail.com>
2019-12-27 18:15:39 +0700

    Edit readme.md & key

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# ongkir
+# API RajaOngkir untuk PHP
+Source PHP untuk menghitung ongkos kirim menggunakan API RajaOngkir
+
+## Fitur
+* Pencarian Ongkos Kirim (JNE, Pos Indonesia, TIKI)
+
+## Konfigurasi
+Keterangan Folder :
+* Starter (Untuk pengguna dengan akun Starter)
+* Basic(Coming soon)
+* Pro (Coming soon)
+
+Source code ini menggunakan PHP Native, Anda hanya perlu mengganti API Key pada file `request.php`
+```
+$key = 'ISI_API_KEY_ANDA'
--- starter/request.php
+++ starter/request.php
@@ -1,5 +1,8 @@
 <?php
+$key = "c46252e90c8e5a91b59bb5a120f78971";
+
 function request_get($url) {
+  $key = $GLOBALS['key'];
   $curl = curl_init();
   curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
   curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -9,7 +12,7 @@ function request_get($url) {
   curl_setopt($curl, CURLOPT_TIMEOUT, 30);
   curl_setopt($curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
   curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
-  curl_setopt($curl, CURLOPT_HTTPHEADER, array("key: c46252e90c8e5a91b59bb5a120f78971"));
+  curl_setopt($curl, CURLOPT_HTTPHEADER, array("key: $key"));
   $result = curl_exec($curl);
   curl_close($curl);
   
@@ -17,6 +20,7 @@ function request_get($url) {
 }
 
 function request_post($url,$data) {
+  $key = $GLOBALS['key'];
   $curl = curl_init();
   curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
   curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -28,7 +32,7 @@ function request_post($url,$data) {
   curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
   curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
   curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-    "key: c46252e90c8e5a91b59bb5a120f78971",
+    "key: $key",
     "content-type: application/x-www-form-urlencoded"
   ));
   $result = curl_exec($curl);
